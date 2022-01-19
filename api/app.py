2e1f8c02bd625996b0d2ceb3e583e806c7353318
Miles <miles@milescrawford.com>
2022-01-18 19:59:39 -0800

    decoding works

--- api/app.py
+++ api/app.py
@@ -1,23 +1,35 @@
 from chalice import Chalice
+from jwt import PyJWKClient
+import json
+import jwt
+import requests
 
 app = Chalice(app_name='joseki-api')
 
-import jwt
-from jwt import PyJWKClient
-url = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_GBQAb61VX/.well-known/jwks.json"
+url = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_EveEoZbAP/.well-known/jwks.json"
 jwks_client = PyJWKClient(url)
 
-
-@app.route('/')
+@app.route('/', cors=True)
 def index():
-    encoded = app.current_request.headers['Authorization']
+    code = app.current_request.headers['Authorization']
+    response = requests.post('https://login.joseki.cat/oauth2/token',{
+        'grant_type': 'authorization_code', 
+        'client_id': '24mjbjvra3522lff13op0dnvhm',  
+        'code': code,
+        'redirect_uri': 'http://localhost:8000/login/'})
+
+    print(response.text);
+    content = json.loads(response.text);
+
+    encoded = content['id_token'] 
+    print(encoded);
     signing_key = jwks_client.get_signing_key_from_jwt(encoded)
     data = jwt.decode(
             encoded,
          signing_key.key,
          algorithms=["RS256"],
-         audience="2j8r5hho8v00qkq3gbns5o456",
+         audience="24mjbjvra3522lff13op0dnvhm",
          options={"verify_exp": False},
      )
-
-    return data
+    print(json.dumps(data));
+    return data;
