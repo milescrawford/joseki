b855e0f178be82a113e1740ea3ccd40d82897a58
Miles <miles@milescrawford.com>
2022-01-25 15:17:56 -0800

    some caching tweaks

---
+++ site/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+terser -c -m -o joseki.js --timings -- joseki.js
+aws s3 cp --cache-control max-age=86400 index.html s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=86400 edit/index.html s3://joseki.milescrawford.com/edit/
+aws s3 cp --cache-control max-age=86400 about/index.html s3://joseki.milescrawford.com/about/
+aws s3 cp --cache-control max-age=86400 login/index.html s3://joseki.milescrawford.com/login/
+aws s3 cp --cache-control max-age=86400 style.css s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=86400 joseki.js s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=86400 wgo.min.js s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=86400 bootstrap.min.css s3://joseki.milescrawford.com/
