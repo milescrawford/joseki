98d8c525e092b7f73c4e6ce3362b8a78eb42f5cd
Robert Christ <robert.christ@gmail.com>
2025-01-20 17:24:01 -0500

    bugfix: Ensures we play the pass on start automatically

--- site/joseki.js
+++ site/joseki.js
@@ -434,7 +434,7 @@ const EMPTY_SCORE = {
         let whiteBegins = [];
         for (const joseki of original.concat(ltr, ttb, diag)){
             let wb = clone(joseki);
-            let passStr = serMove(true, null, null, false);
+            let passStr = serMove(true, null, null, true);
             wb.moves.unshift(passStr);
             whiteBegins.push(wb);
         }
@@ -1108,7 +1108,6 @@ const EMPTY_SCORE = {
             return false;
 
         let m = parseMove(chooseRandomMove(possibleMoves));
-
         if (m.isAuto) {
             if (m.type === PASS) {
                 await passMove(m.isAuto);
