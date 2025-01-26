54d639674ec01356473f8ea0b8ae2dbf07ff70a3
Miles Crawford <miles.crawford@gmail.com>
2025-01-26 09:30:55 -0800

    lower the cache timeout

--- site/deploy.sh
+++ site/deploy.sh
@@ -2,15 +2,15 @@
 set -e
 
 terser -c -m -o joseki.js --timings -- joseki.js
-aws s3 cp --cache-control max-age=86400 index.html s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 edit/index.html s3://joseki.milescrawford.com/edit/
-aws s3 cp --cache-control max-age=86400 about/index.html s3://joseki.milescrawford.com/about/
-aws s3 cp --cache-control max-age=86400 login/index.html s3://joseki.milescrawford.com/login/
-aws s3 cp --cache-control max-age=86400 style.css s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 joseki.js s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 wgo.min.js s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 bootstrap.min.css s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 cat.png s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 favicon.png s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 cat-logo.png s3://joseki.milescrawford.com/
-aws s3 cp --cache-control max-age=86400 manifest.json s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 index.html s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 edit/index.html s3://joseki.milescrawford.com/edit/
+aws s3 cp --cache-control max-age=3600 about/index.html s3://joseki.milescrawford.com/about/
+aws s3 cp --cache-control max-age=3600 login/index.html s3://joseki.milescrawford.com/login/
+aws s3 cp --cache-control max-age=3600 style.css s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 joseki.js s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 wgo.min.js s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 bootstrap.min.css s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 cat.png s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 favicon.png s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 cat-logo.png s3://joseki.milescrawford.com/
+aws s3 cp --cache-control max-age=3600 manifest.json s3://joseki.milescrawford.com/
