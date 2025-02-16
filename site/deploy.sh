ca7eb8ef1d0ce1bdd548d04844aab112b3ebf23f
Miles Crawford <miles.crawford@gmail.com>
2025-02-15 16:35:40 -0800

    stop using terser

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-terser -c -m -o joseki.js --timings -- joseki.js
 aws s3 sync --delete --exclude aws.env --exclude Dockerfile --exclude README.md --exclude deploy.sh . s3://joseki.milescrawford.com/
